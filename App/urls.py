b793c4d0c9fe5e3d83ee224f7aa1178aa426a9ed
chukslord1 <chukslord1@gmail.com>
2020-08-21 17:09:12 +0100

    adding links

--- App/urls.py
+++ App/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path("index.html", views.IndexListView.as_view(), name="index1"),
     path("login-register.html", views.login_register, name="login_register"),
     path("logout.html", views.logout, name="logout"),
-
+    path("submit-property.html", views.submit_property, name="submit_property"),
+    path("listings-list-full-width.html", views.advanced_search, name="advanced_search"),
+    path("listings-grid-standard-with-sidebar.html", views.category, name="category"),
+    path("listings-list-with-sidebar.html", views.popular, name="popular"),
+    path("compare-properties.html", views.compare, name="compare"),
 ]

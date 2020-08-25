b154227b1b38ce7aaee68ba6a5e05cfea30a4f14
chukslord1 <chukslord1@gmail.com>
2020-08-25 22:04:34 +0100

    compare page

--- App/views.py
+++ App/views.py
@@ -172,13 +172,17 @@ def submit_property(request):
         name=request.POST.get("name")
         email=request.POST.get("email")
         phone=request.POST.get("phone")
+        parking=request.POST.get("parking")
+        cooling=request.POST.get("cooling")
+        heating=request.POST.get("heating")
+        sewer=request.POST.get("sewer")
         property_check=Property.objects.filter(title=title,address=address)
         if property_check:
             {message:"Property Already Exists"}
         else:
             property=Property.objects.create(title=title,status=status,category=category,price=price,price_per_unit=price_per_unit,agency=agency,
             area=area,rooms=rooms,image_1=image_1,image_2=image_2,image_3=image_3,image_4=image_4,image_5=image_5,image_6=image_6,image_7=image_7,address=address,
-            description=description,building_age=building_age,bedrooms=bedrooms,bathrooms=bathrooms,features=features)
+            description=description,building_age=building_age,bedrooms=bedrooms,bathrooms=bathrooms,features=features,parking=parking,cooling=cooling,heating=heating,sewer=sewer)
             property.save()
             {message:"Successfully Added Property"}
     return render(request,"submit-property.html",context)
@@ -314,11 +318,74 @@ class SearchListView(ListView):
         return context
 
 
-def category(request):
-    return render(request,"listings-grid-standard-with-sidebar.html")
+class CategoryListView(ListView):
+    model = Property
+    template_name = "listings-grid-standard-with-sidebar.html"
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        if self.request.GET.get('first_check')=="one":
+            query = self.request.GET.get('search')
+            if query:
+                search = self.model.objects.filter(Q(address__icontains=query))
+                context['search'] = search
+            else:
+                search = self.model.objects.none()
+                context['search'] = search
+        elif self.request.GET.get('clear')=="True":
+            clear=Comparison.objects.filter(creator=self.request.user)
+            clear.delete()
+        elif self.request.GET.get('second_check')=="two":
+            if self.request.user.is_authenticated:
+                title=self.request.GET.get('title')
+                address=self.request.GET.get('address')
+                date=self.request.GET.get('date')
+                category=self.request.GET.get('category')
+                sale_type=self.request.GET.get('sale_type')
+                price=self.request.GET.get('price')
+                price_per_unit=self.request.GET.get('price_per_unit')
+                image=self.request.GET.get('image')
+                print(image)
+                image_url=image.replace('/media/','')
+                area=self.request.GET.get('area')
+                rooms=self.request.GET.get('rooms')
+                bedrooms=self.request.GET.get('bedrooms')
+                bathrooms=self.request.GET.get('bathrooms')
+                features=self.request.GET.get('features')
+                building_age=self.request.GET.get('building_age')
+                parking=self.request.GET.get('parking')
+                cooling=self.request.GET.get('cooling')
+                heating=self.request.GET.get('heating')
+                sewer=self.request.GET.get('sewer')
+                water=self.request.GET.get('water')
+                exercise_room=self.request.GET.get('exercise_room')
+                storage_room=self.request.GET.get('storage_room')
+                compare_check=Comparison.objects.filter(title=title,address=address,category=category,sale_type=sale_type,price=price,price_per_unit=price_per_unit,image_1=image_url,
+                rooms=rooms,bedrooms=bedrooms,bathrooms=bathrooms,features=features,building_age=building_age,parking=parking,cooling=cooling,heating=heating,sewer=sewer,
+                water=water,exercise_room=exercise_room,storage_room=storage_room,creator=self.request.user)
+                if compare_check:
+                    pass
+                else:
+                    compare=Comparison.objects.create(title=title,address=address,date=date,category=category,sale_type=sale_type,price=price,price_per_unit=price_per_unit,image_1=image_url,
+                    area=area,rooms=rooms,bedrooms=bedrooms,bathrooms=bathrooms,features=features,building_age=building_age,parking=parking,cooling=cooling,heating=heating,sewer=sewer,
+                    water=water,exercise_room=exercise_room,storage_room=storage_room,creator=self.request.user)
+                    compare.save()
+                compare_count=Comparison.objects.filter(creator=self.request.user    ).count()
+                if compare_count>3:
+                    compare_delete=Comparsion.objects.filter(creator=self.request    .user)[4:]
+                    compare_delete.delete()
+                else:
+                    pass
+        check_login=self.request.user
+        context['houses'] = Property.objects.all()[:6]
+        context['articles'] = Article.objects.all()[:3]
+        context['popular'] = Property.objects.filter(Q(category__icontains="house"))
+        if self.request.user.is_authenticated:
+            context['compare'] = Comparison.objects.filter(creator=self.request.user)
+        else:
+            pass
 
-def popular(request):
-    return render(request,"listings-list-with-sidebar.html")
+        return context
+
 
 class PopularListView(ListView):
     model = Property
@@ -390,7 +457,13 @@ class PopularListView(ListView):
 
 
 def compare(request):
-    return render(request,"compare-properties.html")
+    compare=Comparison.objects.all()
+    context={"compare":compare}
+    if request.GET.get('clear')=="True":
+        clear=Comparison.objects.filter(creator=request.user)
+        clear.delete()
+        return redirect("compare-properties.html")
+    return render(request,"compare-properties.html",context)
 
 def contact(request):
     return render(request,"contact.html")

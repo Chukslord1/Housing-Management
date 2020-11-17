aa3f36527260ed383ca572934af8375c2503aa4c
Chukslord <54282120+Chukslord1@users.noreply.github.com>
2020-11-17 19:49:31 +0100

    Update settings.py

--- Housing_Management/settings.py
+++ Housing_Management/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4%d6w313+$o#q6*sr!6!--)nb83!#u95=2t=^xm8rmjxh+rc=x'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
